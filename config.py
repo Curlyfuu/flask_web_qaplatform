e2abfd72c0a7b84db20f4d9826d5dbf3857d3b76
wake_up_smiling <30908645+wake-up-smiling@users.noreply.github.com>
2019-02-03 21:21:43 +0800

    nothing

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ SECRET_KEY = os.urandom(24)
 
 HOSTNAME = '127.0.0.1'
 PORT     = '3306'
-DATABASE = 'wake_up'
+DATABASE = '***********'
 USERNAME = 'root'
-PASSWORD = '1606260309ww'
+PASSWORD = '**********'
 DB_URI   = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI =DB_URI
